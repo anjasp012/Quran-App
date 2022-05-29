4125d1479cb5e52f4fe7d6d78de05900006126d9
anjas putra <anjas.2104@students.amikom.ac.id>
2022-05-29 17:07:40 +0700

    image test

--- components/DetailCard.js
+++ components/DetailCard.js
@@ -1,22 +1,24 @@
 import React from "react";
 
 export default function DetailCard({ nama, arti, type, ayat }) {
-  return (
-    <header className="mx-6">
-      <div className="relative text-center align-middle bg-gradient-to-br overflow-hidden from-purple-300 via-purple-500 to-purple-700 h-[257px] rounded-2xl">
-        <img
+    return (
+        <header className="mx-6">
+            <div className="relative text-center align-middle bg-gradient-to-br overflow-hidden from-purple-300 via-purple-500 to-purple-700 h-[257px] rounded-2xl">
+                {/* <img
           src="Quran2.png"
           className="absolute opacity-10 -bottom-10 -right-14 select-none"
           alt=""
-        />
-        <h1 className="text-white text-[26px] font-medium mt-5">{nama}</h1>
-        <h3 className="text-white font-medium">{arti}</h3>
-        <hr className="opacity-40 w-8/12 mx-auto my-4" />
-        <p className="text-white  font-medium uppercase">
-          {type} • {ayat} ayat
-        </p>
-        <img src="bismillah.svg" className="mx-auto mt-8" alt="" />
-      </div>
-    </header>
-  );
+        /> */}
+                <h1 className="text-white text-[26px] font-medium mt-5">
+                    {nama}
+                </h1>
+                <h3 className="text-white font-medium">{arti}</h3>
+                <hr className="opacity-40 w-8/12 mx-auto my-4" />
+                <p className="text-white  font-medium uppercase">
+                    {type} • {ayat} ayat
+                </p>
+                {/* <img src="bismillah.svg" className="mx-auto mt-8" alt="" /> */}
+            </div>
+        </header>
+    );
 }
--- components/ListSurat.js
+++ components/ListSurat.js
@@ -1,43 +1,46 @@
 import Link from "next/link";
 
 export default function ListSurat(props, children) {
-  const { surahs } = props;
+    const { surahs } = props;
 
-  return (
-    <section className="mx-6">
-      <h3 className="text-white mb-2 ml-2 mt-8">Daftar Surat</h3>
-      <hr className="mb-6 opacity-30" />
-      <ul className="space-y-4">
-        {surahs.map((surah, index) => {
-          return (
-            <li key={index}>
-              <Link href="/detail">
-                <a className="flex justify-between items-center pb-4 border-b border-white/10">
-                  <div className="flex items-center space-x-5">
-                    <span className="text-white relative p-3.5 text-[14px] font-medium">
-                      <img
+    return (
+        <section className="mx-6">
+            <h3 className="text-white mb-2 ml-2 mt-8">Daftar Surat</h3>
+            <hr className="mb-6 opacity-30" />
+            <ul className="space-y-4">
+                {surahs.map((surah, index) => {
+                    return (
+                        <li key={index}>
+                            <Link href="/detail">
+                                <a className="flex justify-between items-center pb-4 border-b border-white/10">
+                                    <div className="flex items-center space-x-5">
+                                        <span className="text-white relative p-3.5 text-[14px] font-medium">
+                                            {/* <img
                         src="liststyle.svg"
                         className="absolute left-0 top-2 mt-px"
                         alt=""
-                      />
-                      1
-                    </span>
-                    <div>
-                      <h3 className="text-white">{surah.nama}</h3>
-                      <p className="uppercase text-white/60 text-xs">
-                        {surah.type} . {surah.ayat} verses
-                      </p>
-                    </div>
-                  </div>
-                  <h3 className="font-amiri font-bold text-lg text-purple-500">
-                    {surah.asma}
-                  </h3>
-                </a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+                      /> */}
+                                            1
+                                        </span>
+                                        <div>
+                                            <h3 className="text-white">
+                                                {surah.nama}
+                                            </h3>
+                                            <p className="uppercase text-white/60 text-xs">
+                                                {surah.type} . {surah.ayat}{" "}
+                                                verses
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <h3 className="font-amiri font-bold text-lg text-purple-500">
+                                        {surah.asma}
+                                    </h3>
+                                </a>
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
 }
